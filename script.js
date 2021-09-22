df6656b950db3fbd0c9bf7a668b9a09caeea894f
Your Name <you@example.com>
2021-09-22 17:35:38 -0300

    correçao lint script

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
- let buttonLogin = document.getElementById('login');
- let email = document.getElementById('email');
- let senha = document.getElementById('senha');
- let valorEmail = 'tryber@teste.com';
- let valorSenha = '123456';
+const buttonLogin = document.getElementById('login');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const valorEmail = 'tryber@teste.com';
+const valorSenha = '123456';
 
- function verificaImput() {
-  if (email.value === valorEmail 
+function verificaImput() {
+  if (email.value === valorEmail
   && senha.value === valorSenha) {
     alert('Olá, Tryber!');
   } else {
-  alert('Email ou senha inválidos.');
+    alert('Email ou senha inválidos.');
   }
 }
 
 buttonLogin.addEventListener('click', verificaImput);
-
